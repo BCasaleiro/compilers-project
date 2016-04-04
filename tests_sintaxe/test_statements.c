2043398be13cc8a67b7ee1c84790d0415cf4a1b9
Bernardo Casaleiro <bernardocasaleiro@gmail.com>
2016-04-04 03:45:44 +0100

    added more test cases

---
+++ tests_sintaxe/test_statements.c
@@ -0,0 +1,140 @@
+char buffer[20];
+int main(int argc, char **argv) {
+    if(a == b){}
+
+    if(a == b){}else ;
+
+    if(c == d)
+    	a + b;
+    else
+    	d + o;
+
+    if(c2 == d3){
+    	a + t;
+    	v + 4;
+    }
+
+    if(c2 == d3){
+    	a + t;
+    	v + 4;
+    }else{
+    	sdf = sd +5;
+    }
+
+    if (ass == ssa){
+    	;;
+    	a + b; 
+    	;
+    	;;
+    	a + c;
+    	;
+    	d + o;
+    	d + y;
+    	;;;
+    	{a = 2;};
+    	{}{}
+    	{{}}
+    }
+
+    for (i = 0; i < count; i = i + 1){
+    	if(a == b){
+    		for (;;i = i+1)
+    		{
+    			if(c2 == d3){
+			    	a + t;
+			    	v + 4;
+			    }else{
+			    	sdf = sd +5;
+			    }
+
+			    if (ass == ssa){
+			    	;;
+			    	a + b; 
+			    	;
+			    	;;
+			    	a + c;
+			    	;
+			    	d + o;
+			    	d + y;
+			    	;;;
+			    	{a = 2;};
+			    	{jht+1;}{}
+			    	{{}}
+			    	{{}a = b + c; }
+			    }
+    			
+    		}
+    	}
+
+    	for ( ; ; ){}
+    	for ( ; ; ){
+    		a + 1;
+    		s + 4;
+    	}
+
+    	for ( ; ; ){
+    		if (ass == ssa * nt){
+		    	;;
+		    	a + b; 
+		    	;
+		    	;;
+		    	a + c;
+		    	;
+		    	d + o;
+		    	d + y;
+		    	;;;
+		    	{a = 2;};
+		    	{}{}
+		    	{{}}
+			}
+			;
+			{}
+			{{a = g + t;}}
+
+            if(a == t){
+                sdf = d + 1;
+            }
+			
+    	}
+        ;
+        {{}}
+        ;a(1, 'a', *a, ***a, **a, *func(s[5],s,*v), i[a]);
+
+        ;
+        {};
+
+        
+
+        if(a==b){if(a==b){if(a==b){}else{}}else{if(a==b){if(a==b){if(a==b){if(a==b){if(a==b){};}}};};} c = a + b;}else{if(a==b){if(a==b){}else{}}else{if(a==b){}else{}}}
+
+        ;
+
+    	a = "sdfsdfsdf";
+    	b = 's';
+    	c = 1;
+
+    	{}
+    	return;
+    	return 1;
+    	return a(1, w, 3, a);
+
+
+
+
+
+
+    }
+
+
+    -a*-b;
+    -a+-b;
+    -a*b;
+    +a*+b;
+    -a/-b;
+    *a[2];
+    a(1,b,*a,&as);
+    **a(1,b,*a,&as);
+
+	return 0; 
+
+}
